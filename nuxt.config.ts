63a915090558b5732dc4a18fbedc501135c8208a
DonValen910 <valentinstito@gmail.com>
2024-12-11 00:21:44 -0300

    Agregar configuración y plugin de Supabase en la aplicación Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  css: ['./assets/main.css'],
   devtools: { enabled: true },
   modules: ['@nuxtjs/supabase'],
-  css: ['./assets/main.css']
+  plugins: ['~/plugins/supabase'],
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY
+    }
+  },
+  supabase: {
+    redirect: false,
+  }
 })

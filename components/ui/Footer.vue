b9c5ffb6bd84ef29778cb44b5e7e33352f6036a3
DonValen910 <valentinstito@gmail.com>
2025-01-19 18:02:17 -0300

    Agregar componente Footer con enlaces legales y redes sociales

---
+++ components/ui/Footer.vue
@@ -0,0 +1,55 @@
+<template>
+  <footer class="footer">
+    <div class="footer__content">
+      <NuxtLink to="/" class="footer__logo-link">
+        <img src="/assets/ByAriana-transparente.png" alt="Logo ByAriana" class="footer__logo">
+      </NuxtLink>
+      <div class="footer__legal">
+        <h4 class="footer__legal-title">Avisos Legales</h4>
+        <span class="footer__legal-item">Políticas de privacidad</span>
+        <span class="footer__legal-item">Términos de uso</span>
+        <span class="footer__legal-item">© ByAriana | Todos los derechos reservados</span>
+      </div>
+      <div class="footer__social">
+        <NuxtLink to="/" class="footer__social-link">
+          <img src="/assets/redes/whatsapp.svg" alt="" class="footer__social-icon">
+        </NuxtLink>
+        <NuxtLink to="/" class="footer__social-link">
+          <img src="/assets/redes/instagram.svg" alt="" class="footer__social-icon">
+        </NuxtLink>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+.footer {
+  background: #E1B2E6;
+  width: 100%;
+  height: 10.625rem;
+}
+
+.footer__content {
+  max-width: 56.8125rem;
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  justify-content: space-between;
+}
+
+.footer_legal {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.footer__social {
+  display: flex;
+  flex-direction: column;
+}
+</style>

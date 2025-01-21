8c10a8a9d08cc2b204128cff037901e6005da7df
Valentín Tito <131624715+DonValen910@users.noreply.github.com>
2025-01-21 01:37:32 +0000

    Mejorar el diseño del pie de página: ajustar estilos de avisos legales y agregar efecto hover en iconos sociales

--- components/ui/Footer.vue
+++ components/ui/Footer.vue
@@ -5,10 +5,10 @@
         <img src="/assets/ByAriana-transparente.png" alt="Logo ByAriana" class="footer__logo">
       </NuxtLink>
       <div class="footer__legal">
-          <h4 class="footer__legal-title">Avisos Legales</h4>
-          <span class="footer__legal-item">Políticas de privacidad</span>
-          <span class="footer__legal-item">Términos de uso</span>
-          <span class="footer__legal-item">© ByAriana | Todos los derechos reservados</span>
+        <h4 class="footer__legal-title">Avisos Legales</h4>
+        <span class="footer__legal-item">Políticas de privacidad</span>
+        <span class="footer__legal-item">Términos de uso</span>
+        <span class="footer__legal-derechos">© ByAriana | Todos los derechos reservados</span>
       </div>
       <div class="footer__social">
         <a href="https://wa.me/5491138950514" target="_blank" class="footer__social-link">
@@ -30,7 +30,6 @@
 .footer {
   background: #E1B2E6;
   width: 100%;
-  height: 10.625rem;
 }
 
 .footer__content {
@@ -42,10 +41,23 @@
 }
 
 .footer__legal {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
+  display: grid;
+  justify-items: center;
+  gap: 15px;
+}
+
+.footer__legal-title {
+  font-weight: bold;
+  font-size: 28px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.footer__legal-item {
+  font-size: 24px;
+}
+
+.footer__legal-derechos {
+  font-size: 20px;
 }
 
 .footer__social {
@@ -56,5 +68,10 @@
 .footer__social-icon {
   width: 4rem;
   height: 4rem;
+  transition: .3s;
+}
+
+.footer__social-icon:hover {
+  transform: scale(1.1);
 }
 </style>

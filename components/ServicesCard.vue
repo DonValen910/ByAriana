c8f0de38d807409bf3802d530d3a9dd7b13d2513
Valentín Tito <131624715+DonValen910@users.noreply.github.com>
2025-02-19 20:40:55 +0000

    Agregar componente ServicesCard.vue y actualizar servicios.vue para mostrar tratamientos con tarjetas

---
+++ components/ServicesCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card">
+    <img class="card__image" :src="url" :alt="alt">
+    <h3 class="card__title">{{ title }}</h3>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  url: String,
+  alt: String,
+  title: String,
+})
+</script>
+
+<style>
+.card {
+  width: 325px;
+  height: 325px;
+  cursor: pointer;
+  transition: all .5s;
+}
+
+.card:hover {
+  transform: scale(1.05);
+}
+
+.card__image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 1rem;
+}
+
+.card__title {
+  position: relative;
+  padding: 0 0 0.7rem 1.25rem;
+  bottom: 2.6rem;
+  background: linear-gradient(0deg, rgba(225, 178, 230, 1) 30%, rgba(225, 178, 230, 0.63) 75%, rgba(225, 178, 230, 0.05) 100%);
+  border-radius: 0 0 1rem 1rem;
+  font-weight: 500;
+  font-size: 1.6rem;
+}
+</style>
--- pages/servicios.vue
+++ pages/servicios.vue
@@ -1,15 +1,65 @@
 <template>
-  <div>
+  <main class="main">
+    <h1 class="main__title">Descubre Nuestros Tratamientos</h1>
+
+    <section class="main__section main__section--manicuria">
+      <h2 class="main__section-title">Manicuría</h2>
+      <div class="main__section-content">
+        <Card url="/resultado/Manos/mano_7.webp" alt="Imagen de manicuría común" title="Común" />
+        <Card url="/resultado/Manos/mano_8.webp" alt="Imagen de manicuría semipermanente" title="Semipermanente" />
+        <Card url="/resultado/Manos/mano_6.webp" alt="Imagen de manicuría capping" title="Capping" />
+        <Card url="/resultado/Manos/mano_9.webp" alt="Imagen de manicuría services" title="Services" />
+        <Card url="/resultado/Manos/mano_10.webp" alt="Imagen de manicuría esculpidas" title="Esculpidas" />
+      </div>
+    </section>
     
-  </div>
+    <section class="main__section main__section--pedicuria">
+      <h2 class="main__section-title">Pedicuría</h2>
+    </section>
+
+    <section class="main__section main__section--lifting">
+      <h2 class="main__section-title">Lifting</h2>
+    </section>
+
+    <section class="main__section main__section--facial">
+      <h2 class="main__section-title">Facial</h2>
+    </section>
+
+    <section class="main__section main__section--microblading">
+      <h2 class="main__section-title">Microblading</h2>
+    </section>
+
+    <section class="main__section main__section--masajes">
+      <h2 class="main__section-title">Masajes</h2>
+    </section>
+  </main>
 </template>
 
 <script lang="ts" setup>
+import Card from '~/components/ServicesCard.vue';
+
 useHead({
   title: 'Servicios',
 })
 </script>
 
 <style>
+.main__title {
+  text-align: center;
+  font-size: 3rem;
+  margin: 2.3rem 1rem;
+}
 
+.main__section-title {
+  font-size: 2.2rem;
+  font-weight: 500;
+  margin: 1rem;
+}
+
+.main__section-content {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2.5rem;
+}
 </style>

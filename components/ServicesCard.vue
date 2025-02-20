0b0d4741cda7a685e2f8f7e4888198561b977aba
Valentín Tito <131624715+DonValen910@users.noreply.github.com>
2025-02-20 21:32:12 +0000

    Actualizar componente ServicesCard.vue para mejorar el diseño y la interactividad con un botón de "Más info"

--- components/ServicesCard.vue
+++ components/ServicesCard.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="card">
     <img class="card__image" :src="url" :alt="alt">
-    <h3 class="card__title">{{ title }}</h3>
+    <div class="card__content">
+      <h3 class="card__content-title">{{ title }}</h3>
+      <button class="card__content-btn">Más info</button>
+    </div>
   </div>
 </template>
 
@@ -17,12 +20,13 @@ defineProps({
 .card {
   width: 325px;
   height: 325px;
-  cursor: pointer;
   transition: all .5s;
+  border-radius: 1rem;
 }
 
 .card:hover {
   transform: scale(1.05);
+  box-shadow: 0 0 15px #000000;
 }
 
 .card__image {
@@ -32,13 +36,33 @@ defineProps({
   border-radius: 1rem;
 }
 
-.card__title {
-  position: relative;
-  padding: 0 0 0.7rem 1.25rem;
-  bottom: 2.6rem;
-  background: linear-gradient(0deg, rgba(225, 178, 230, 1) 30%, rgba(225, 178, 230, 0.63) 75%, rgba(225, 178, 230, 0.05) 100%);
+.card__content {
   border-radius: 0 0 1rem 1rem;
+  background: linear-gradient(0deg, rgba(225, 178, 230, 1) 30%, rgba(225, 178, 230, 0.63) 75%, rgba(225, 178, 230, 0.05) 100%);
+  padding: 0 1.25rem 0.7rem 1.25rem;
+  position: relative;
+  bottom: 2.6rem;
+  display: flex;
+  justify-content: space-between;
+}
+
+.card__content-title {
   font-weight: 500;
   font-size: 1.6rem;
+  display: inline-block;
+}
+
+.card__content-btn {
+  border: none;
+  padding: 0.5rem;
+  border-radius: 8px;
+  background: #703e78;
+  color: white;
+  cursor: pointer;
+  transition: all .3s;
+}
+
+.card__content-btn:hover {
+  background: #5c2f5c;
 }
 </style>

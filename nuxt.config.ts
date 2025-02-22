283c5e21567522260e3d9bbbe58967c343698aa1
Valentín Tito <131624715+DonValen910@users.noreply.github.com>
2025-02-22 04:04:09 +0000

    Eliminar configuración y dependencias de Supabase en nuxt.config.ts y package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,17 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   css: ['./assets/main.css'],
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase'],
-  plugins: ['~/plugins/supabase'],
-  runtimeConfig: {
-    public: {
-      supabaseUrl: process.env.SUPABASE_URL,
-      supabaseKey: process.env.SUPABASE_KEY
-    }
-  },
-  supabase: {
-    redirect: false,
-  },
+  modules: [],
+  plugins: [],
   vite: {
     server: {
       watch: {

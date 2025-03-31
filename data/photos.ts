bebc78bac26d4db012f730e60695c052c20ee1c9
DonValen910 <valentinstito@gmail.com>
2025-03-30 22:56:30 -0300

    Agregar archivo de fotos con imágenes de manicuria, lifting, pedicuria, facial y perfilado

---
+++ data/photos.ts
@@ -0,0 +1,300 @@
+const pathManos = "public/resultado/Manos"
+const pathLifting = "public/resultado/Lifting"
+const pathPies = "public/resultado/Pies"
+const pathFacial = "public/resultado/Facial"
+const pathPerfilado = "public/resultado/Perfilado"
+
+export const PHOTOS = [
+  {
+    manos: [
+      {
+        id: '1',
+        title: 'Manicuria',
+        description: 'Imagen de manicuria',
+        url: `${pathManos}/mano_1.webp`,
+      },
+      {
+        id: '2',
+        title: 'Manicuria',
+        description: 'Imagen de manicuria',
+        url: `${pathManos}/mano_2.webp`,
+      },
+      {
+        id: '3',
+        title: 'Manicuria',
+        description: 'Imagen de manicuria',
+        url: `${pathManos}/mano_3.webp`,
+      },
+      {
+        id: '4',
+        title: 'Manicuria',
+        description: 'Imagen de manicuria',
+        url: `${pathManos}/mano_4.webp`,
+      },
+      {
+        id: '5',
+        title: 'Manicuria',
+        description: 'Imagen de manicuria',
+        url: `${pathManos}/mano_5.webp`,
+      },
+      {
+        id: '6',
+        title: 'Manicuria',
+        description: 'Imagen de manicuria',
+        url: `${pathManos}/mano_6.webp`,
+      },
+      {
+        id: '7',
+        title: 'Manicuria',
+        description: 'Imagen de manicuria',
+        url: `${pathManos}/mano_7.webp`,
+      },
+      {
+        id: '8',
+        title: 'Manicuria',
+        description: 'Imagen de manicuria',
+        url: `${pathManos}/mano_8.webp`,
+      },
+      {
+        id: '9',
+        title: 'Manicuria',
+        description: 'Imagen de manicuria',
+        url: `${pathManos}/mano_9.webp`,
+      },
+      {
+        id: '10',
+        title: 'Manicuria',
+        description: 'Imagen de manicuria',
+        url: `${pathManos}/mano_10.webp`,
+      }
+    ]
+  },
+  {
+    lifting: [
+      {
+        id: '1',
+        title: 'Lifting',
+        description: 'Imagen de lifting',
+        url: `${pathLifting}/lifting_1.webp`,
+      },
+      {
+        id: '2',
+        title: 'Lifting',
+        description: 'Imagen de lifting',
+        url: `${pathLifting}/lifting_2.webp`,
+      },
+      {
+        id: '3',
+        title: 'Lifting',
+        description: 'Imagen de lifting',
+        url: `${pathLifting}/lifting_3.webp`,
+      },
+      {
+        id: '4',
+        title: 'Lifting',
+        description: 'Imagen de lifting',
+        url: `${pathLifting}/lifting_4.webp`,
+      },
+      {
+        id: '5',
+        title: 'Lifting',
+        description: 'Imagen de lifting',
+        url: `${pathLifting}/lifting_5.webp`,
+      },
+      {
+        id: '6',
+        title: 'Lifting',
+        description: 'Imagen de lifting',
+        url: `${pathLifting}/lifting_6.webp`,
+      },
+      {
+        id: '7',
+        title: 'Lifting',
+        description: 'Imagen de lifting',
+        url: `${pathLifting}/lifting_7.webp`,
+      },
+      {
+        id: '8',
+        title: 'Lifting',
+        description: 'Imagen de lifting',
+        url: `${pathLifting}/lifting_8.webp`,
+      },
+      {
+        id: '9',
+        title: 'Lifting',
+        description: 'Imagen de lifting',
+        url: `${pathLifting}/lifting_9.webp`,
+      },
+      {
+        id: '10',
+        title: 'Lifting',
+        description: 'Imagen de lifting',
+        url: `${pathLifting}/lifting_10.webp`,
+      },
+      {
+        id: '11',
+        title: 'Lifting',
+        description: 'Imagen de lifting',
+        url: `${pathLifting}/lifting_11.webp`,
+      },
+      {
+        id: '12',
+        title: 'Lifting',
+        description: 'Imagen de lifting',
+        url: `${pathLifting}/lifting_12.webp`,
+      },
+      {
+        id: '13',
+        title: 'Lifting',
+        description: 'Imagen de lifting',
+        url: `${pathLifting}/lifting_13.webp`,
+      },
+      {
+        id: '14',
+        title: 'Lifting',
+        description: 'Imagen de lifting',
+        url: `${pathLifting}/lifting_14.webp`,
+      },
+      {
+        id: '15',
+        title: 'Lifting',
+        description: 'Imagen de lifting',
+        url: `${pathLifting}/lifting_15.webp`,
+      },
+      {
+        id: '16',
+        title: 'Lifting',
+        description: 'Imagen de lifting',
+        url: `${pathLifting}/lifting_16.webp`,
+      },
+      {
+        id: '17',
+        title: 'Lifting',
+        description: 'Imagen de lifting',
+        url: `${pathLifting}/lifting_17.webp`,
+      }
+    ]
+  },
+  {
+    pies: [
+      {
+        id: '1',
+        title: 'Pedicuria',
+        description: 'Imagen de pedicuria',
+        url: `${pathPies}/pie_1.webp`,
+      },
+      {
+        id: '2',
+        title: 'Pedicuria',
+        description: 'Imagen de pedicuria',
+        url: `${pathPies}/pie_2.webp`,
+      },
+      {
+        id: '3',
+        title: 'Pedicuria',
+        description: 'Imagen de pedicuria',
+        url: `${pathPies}/pie_3.webp`,
+      },
+      {
+        id: '4',
+        title: 'Pedicuria',
+        description: 'Imagen de pedicuria',
+        url: `${pathPies}/pie_4.webp`,
+      },
+      {
+        id: '5',
+        title: 'Pedicuria',
+        description: 'Imagen de pedicuria',
+        url: `${pathPies}/pie_5.webp`,
+      },
+      {
+        id: '6',
+        title: 'Pedicuria',
+        description: 'Imagen de pedicuria',
+        url: `${pathPies}/pie_6.webp`,
+      },
+      {
+        id: '7',
+        title: 'Pedicuria',
+        description: 'Imagen de pedicuria',
+        url: `${pathPies}/pie_7.webp`,
+      },
+      {
+        id: '8',
+        title: 'Pedicuria',
+        description: 'Imagen de pedicuria',
+        url: `${pathPies}/pie_8.webp`,
+      },
+      {
+        id: '9',
+        title: 'Pedicuria',
+        description: 'Imagen de pedicuria',
+        url: `${pathPies}/pie_9.webp`,
+      },
+      {
+        id: '10',
+        title: 'Pedicuria',
+        description: 'Imagen de pedicuria',
+        url: `${pathPies}/pie_10.webp`,
+      }
+    ]
+  },
+  {
+    facial: [
+      {
+        id: '1',
+        title: 'Facial',
+        description: 'Imagen de facial',
+        url: `${pathFacial}/facial_1.webp`,
+      }
+    ]
+  },
+  {
+    perfilado: [
+      {
+        comun: [
+          {
+            id: '1',
+            title: 'Perfilado común',
+            description: 'Imagen de perfilado común',
+            url: `${pathPerfilado}/Comun/perfilado_comun_1.webp`,
+          },
+          {
+            id: '2',
+            title: 'Perfilado común',
+            description: 'Imagen de perfilado común',
+            url: `${pathPerfilado}/Comun/perfilado_comun_2.webp`,
+          },
+          {
+            id: '3',
+            title: 'Perfilado común',
+            description: 'Imagen de perfilado común',
+            url: `${pathPerfilado}/Comun/perfilado_comun_3.webp`,
+          }
+        ]
+      },
+      {
+        henna: [
+          {
+            id: '1',
+            title: 'Perfilado con henna',
+            description: 'Imagen de perfilado con henna',
+            url: `${pathPerfilado}/Henna/perfilado_henna_1.webp`,
+          },
+          {
+            id: '2',
+            title: 'Perfilado con henna',
+            description: 'Imagen de perfilado con henna',
+            url: `${pathPerfilado}/Henna/perfilado_henna_2.webp`,
+          },
+          {
+            id: '3',
+            title: 'Perfilado con henna',
+            description: 'Imagen de perfilado con henna',
+            url: `${pathPerfilado}/Henna/perfilado_henna_3.webp`,
+          }
+        ]
+      }
+    ]
+  }
+]
